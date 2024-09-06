9c9e908813fbca0a9101f53457b6b52788bed65e
Ashi Mughal <150792836+Ashi673@users.noreply.github.com>
2024-09-06 07:18:01 +0500

    layout

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('index');
+
+Route::get('/privacy', function () {
+    return view('privacy');
+})->name('privacy');
+
+Route::get('/term', function () {
+    return view('term');
+})->name('term');
+
+Route::get('/project', function () {
+    return view('project');
+})->name('project');
