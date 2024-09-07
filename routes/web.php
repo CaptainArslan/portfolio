921edbd835e43800659f33db04e22e813df6e20e
Muhammad Arslan <mughalarslan996@gmail.com>
2024-09-07 16:05:53 +0500

    setup home page dynamic app blade

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-})->name('index');
+Route::get('/', [FrontendController::class, 'index']);
+Route::get('home', [FrontendController::class, 'index'])->name('home');
 
 Route::get('/privacy', function () {
     return view('privacy');
